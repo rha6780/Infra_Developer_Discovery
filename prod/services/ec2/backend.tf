da3df6c1c047730435fddb74e389a964e5be8b2e
rha6780 <rha4578@gmail.com>
2023-05-10 22:01:52 +0900

    feat: ec2 기본 파일 추가

---
+++ prod/services/ec2/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+    backend "s3"{
+        bucket  = "infra-developer-discovery"
+        key     = "prod/services/ec2/terraform.tfstate"
+        profile = "ddprod"
+        region  = "ap-northeast-2"
+    }
+}
