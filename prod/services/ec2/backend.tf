8c4bdc5892d9bd07617bf9f98ad5c162586e8352
rha6780 <rha4578@gmail.com>
2023-05-11 20:52:42 +0900

    feat: ec2 관련 코드 추가

--- prod/services/ec2/backend.tf
+++ prod/services/ec2/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-    backend "s3"{
-        bucket  = "infra-developer-discovery"
-        key     = "prod/services/ec2/terraform.tfstate"
-        profile = "ddprod"
-        region  = "ap-northeast-2"
-    }
+  backend "s3" {
+    bucket  = "infra-developer-discovery"
+    key     = "prod/services/ec2/terraform.tfstate"
+    profile = "ddprod"
+    region  = "ap-northeast-2"
+  }
 }
--- prod/services/ec2/main.tf
+++ prod/services/ec2/main.tf
@@ -1,1 +1,27 @@
 #TODO : ec2 리소스 생성 구문 작성 (ami 등)
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "developer_discovery_api"{
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name  = "developer_discovery"
+    Stage = "prod"
+  }
+}
