3100c6c1b8410e755775bd0be305a229c86456cb
rha6780 <rha4578@gmail.com>
2023-06-03 22:18:46 +0900

    feat: output private_key with sensitive

--- prod/services/ec2/outputs.tf
+++ prod/services/ec2/outputs.tf
@@ -2,3 +2,9 @@ output "ec2_arn" {
     description = "enviroment variable of codedeploy target ec2 arn"
     value = aws_instance.developer_discovery_api.arn
 }
+
+output "private_key" {
+    description = "enviroment variable of ec2 pem key"
+    value     = tls_private_key.private_key.private_key_pem
+    sensitive = true
+}
