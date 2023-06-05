0d1d388026384450e83badc7d8ee01ae03f40204
rha6780 <rha4578@gmail.com>
2023-06-05 21:49:32 +0900

    feat: ecr repository add

--- prod/services/ecr/main.tf
+++ prod/services/ecr/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "developer_discovery" {
-  name                 = "developer_discovery"
+  name                 = "developer_discovery_repository"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
